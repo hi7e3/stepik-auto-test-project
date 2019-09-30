1c5922f42e7bc6eae8286e634d551eec6b36b264
Le Gr <55333954+hi7e3@users.noreply.github.com>
2019-09-30 21:28:50 +0200

    test_guest_can_go_to_login_page edited. locators restored

--- pages/locators.py
+++ pages/locators.py
@@ -5,5 +5,5 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     
 class LoginPageLocators():
-    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form_invalid")
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form_invalid")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
