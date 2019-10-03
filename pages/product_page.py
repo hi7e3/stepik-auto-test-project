ca406eb208212a0086dd9fe4d9a05474745d1d07
Le Gr <55333954+hi7e3@users.noreply.github.com>
2019-10-03 22:32:21 +0200

    added product page testing

---
+++ pages/product_page.py
@@ -0,0 +1,30 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def press_add_to_basket(self):
+        link = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        link.click()
+    
+    def get_product_name(self):
+        prodname_element = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+        #print("product name: ", prodname_element.text)
+        return(prodname_element.text)
+        
+    def get_product_price(self):
+        prodprice_element = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        #print("product price: ", prodprice_element.text)
+        return(prodprice_element.text)
+    
+    def should_basket_name_be_equal_product_name(self, pr_name):
+    #Сообщение о том, что товар добавлен в корзину. 
+    #Название товара в сообщении должно совпадать с тем товаром, который вы действительно добавили
+        prodname_basket_element = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_BASKET_MESSAGE)
+        #print("product name basket message: ", prodname_basket_element.text)
+        assert pr_name == prodname_basket_element.text
+
+    def should_basket_price_be_equal_product_price(self, pr_price):
+    #Сообщение со стоимостью корзины. Стоимость корзины совпадает с ценой товара
+        prodprice_basket_element = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_BASKET_MESSAGE)
+        #print("product price basket message: ", prodprice_basket_element.text)
+        assert pr_price == prodprice_basket_element.text
---
+++ test_product_page.py
@@ -0,0 +1,26 @@
+import pytest
+from pages.main_page import MainPage
+from pages.product_page import ProductPage
+import time
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    prod_name = page.get_product_name()
+    prod_price = page.get_product_price()
+    page.press_add_to_basket()
+    page.solve_quiz_and_get_code()
+    time.sleep(15)
+    page.should_basket_name_be_equal_product_name(prod_name)
+    page.should_basket_price_be_equal_product_price(prod_price)
